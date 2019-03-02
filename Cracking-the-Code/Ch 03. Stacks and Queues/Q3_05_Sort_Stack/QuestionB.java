44b1bcfdd52eb79bb9cc4a503f21b58e25178fc6
Ed Bayudan <ed.bayudan@yahoo.com>
2019-03-02 17:20:11 -0500

    Fix sort stack

--- Cracking-the-Code/Ch 03. Stacks and Queues/Q3_05_Sort_Stack/QuestionB.java
+++ Cracking-the-Code/Ch 03. Stacks and Queues/Q3_05_Sort_Stack/QuestionB.java
@@ -25,22 +25,39 @@ public class QuestionB {
 		}
 	}
 			
+	/**
+	 * Go through the original stack
+	 * Pop the top element and save to 'tmp'
+	 * Now go through the second stack
+	 * If the second stack is empty, push the 'tmp' element to it
+	 * If not empty,pop every element in the second stack that is GREATER than the 'tmp' element
+	 * Then push these back to the original stack.
+	 * Then push the 'tmp' element to the second stack
+	 * Follow this for the rest of the elements in the original stack
+	 * 
+	 * The idea is to set aside an element from the original stack and insert it in the second stack, 
+	 * the smallest being at the bottom
+	 * 
+	 * Then you can put everything back in the original stack, with the largest going to the bottom
+	 * 
+	 * Note how the stack and deque store the elements differently. However, pop() work the same way 
+	 * 
+	 * @param s
+	 */
 	public static void sortDQStack(Deque<Integer> s) {
 		Deque<Integer> r = new ArrayDeque<Integer>();
-		System.out.println("\nSORTING:\n");
+		//System.out.println("\nSORTING:\n");
 		while(!s.isEmpty()) {
-			printDeque(s, "s");
-			printDeque(r, "r");
-			System.out.println("");
+			//printDeque(s, "original:");
+			//printDeque(r, "secondary:");
 			int tmp = s.pop();
 			while(!r.isEmpty() && r.peek() > tmp) {
 				s.push(r.pop());
 			}
 			r.push(tmp);
 		}
-		printDeque(s, "s");
-		printDeque(r, "r");
-		System.out.println("");
+		//printDeque(s, "original:");
+		//printDeque(r, "secondary:");
 		
 		/* Copy the elements back. */
 		while (!r.isEmpty()) {
@@ -50,44 +67,39 @@ public class QuestionB {
 		public static void main(String [] args) {
 		Stack<Integer> s = new Stack<Integer>();
 		Deque<Integer> dqstack = new ArrayDeque<Integer>();
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 10; i++) {
 			int r = AssortedMethods.randomIntInRange(0,  1000);
 			s.push(r);
 			dqstack.push(r);
 		}
 
-		printStack(s, "original stack");
-		System.out.println("");
+		printStack(s,"original stack");
 		sortStack(s);
-		printStack(s, "sorted stack");
-		System.out.println("");
-		popStack(s, "popped stack");
+		printStack(s,"sorted stack");
+		popStack(s,"popped stack");
 		System.out.println("");
 
-		printDeque(dqstack, "original deque");
-		System.out.println("");
+		printDeque(dqstack,"original deque");
 		sortDQStack(dqstack);
-		printDeque(dqstack, "sorted deque");
-		System.out.println("");
-		popDeque(dqstack, "popped deque");
-		System.out.println("");
+		printDeque(dqstack,"sorted deque");
+		popDeque(dqstack,"popped deque");
 
 	}
 	
-	public static void printStack(Stack<Integer> s, String desc) {
+	public static void printStack(Stack<Integer> stk, String desc) {
 		System.out.println("");  
 		System.out.println("\n" + desc);
-		Iterator<Integer> q=s.iterator();  
-		while(q.hasNext())  
+		Iterator<Integer> is=stk.iterator();  
+		while(is.hasNext())  
 		{  
-			System.out.print(q.next() + ":");  
+			System.out.print(is.next() + ":");  
 		}
 		
 	}
-	public static void printDeque(Deque<Integer>  s, String desc) {
+	public static void printDeque(Deque<Integer>  que, String desc) {
 		System.out.println("");  
 		System.out.println("\n" + desc);
-		Iterator<Integer> q=s.iterator();  
+		Iterator<Integer> q=que.iterator();  
 		while(q.hasNext())  
 		{  
 			System.out.print(q.next() + ":");  
@@ -95,6 +107,7 @@ public class QuestionB {
 		
 	}
 	public static void popStack(Stack<Integer> s, String desc) {
+		System.out.println("");  
 		System.out.println("\n" + desc);
 		while(!s.isEmpty()) {
 			System.out.print(s.pop() + ":");
@@ -102,6 +115,7 @@ public class QuestionB {
 		
 	}
 	public static void popDeque(Deque<Integer>  s, String desc) {
+		System.out.println("");  
 		System.out.println("\n" + desc);
 		while(!s.isEmpty()) {
 			System.out.print(s.pop() + ":");
